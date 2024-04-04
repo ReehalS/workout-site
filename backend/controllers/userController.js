eec3ad5f8da5fa2659a926e5fb14c6292e90e3da
SandeepR <sunny.reehal@gmail.com>
2024-04-04 11:19:06 -0700

    Adding tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+
+const crearteToken = (_id) => {
+    return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: '30d'})
+}
 
 
 const loginUser = async (req, res) => {
@@ -9,7 +15,10 @@ const signupUser = async (req, res) => {
     const [email, password] = [req.body.email, req.body.password]
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email,user})
+
+        const token = crearteToken(user._id)
+
+        res.status(200).json({email,token})
     } catch (error) {
         res.status(400).json({message: error.message})
     }
