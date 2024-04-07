8311ecb0e82ee0164bd7e9f78d4be1bccb12aed9
SandeepR <sunny.reehal@gmail.com>
2024-04-06 19:25:22 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,20 +8,21 @@ const userRoutes = require('./routes/user.js')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
-// const allowedOrigins = ['http://localhost:3000'];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('The CORS policy for this site does not allow access from the specified origin.'), false);
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
+const allowedOrigins = ['http://localhost:3000', 'https://workoutbuddy-sr.vercel.app'];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.some(allowedOrigin => origin.startsWith(allowedOrigin))) {
+      callback(null, true);
+    } else {
+      callback(new Error('The CORS policy for this site does not allow access from the specified origin.'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 
 
 app.use((req, res, next) => {
