89e6800aa9097a49433bfe1857b708c462980f33
SandeepR <sunny.reehal@gmail.com>
2024-04-06 18:09:54 -0700

    update package.json

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,7 @@ const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
 const app = express()
-
 app.use(express.json())
-
 app.use(cors())
 
 // const allowedOrigins = ['http://localhost:3000'];
